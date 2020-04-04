626a3f120f7cfcfa67d9ee4dd097dec417c6792f
nicholas landreville <nl@nicholass-MacBook-Air.local>
2020-04-04 16:49:22 -0500

    Updates

---
+++ precache-manifest.b3612d67e53bfb405f56421050766f95.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76d5ecaee6a421735aa290e7c16458f",
+    "url": "/updated-portfolio/index.html"
+  },
+  {
+    "revision": "ddbd550d0b70d695e6fc",
+    "url": "/updated-portfolio/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "d07862d7de184397fa00",
+    "url": "/updated-portfolio/static/css/main.695006c5.chunk.css"
+  },
+  {
+    "revision": "ddbd550d0b70d695e6fc",
+    "url": "/updated-portfolio/static/js/2.815a3798.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/updated-portfolio/static/js/2.815a3798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d07862d7de184397fa00",
+    "url": "/updated-portfolio/static/js/main.48e1df90.chunk.js"
+  },
+  {
+    "revision": "6e9e911a425a309a0a41",
+    "url": "/updated-portfolio/static/js/runtime-main.dac43366.js"
+  },
+  {
+    "revision": "f374840dccdffe46626d3c956f2ba4eb",
+    "url": "/updated-portfolio/static/media/pic1.f374840d.png"
+  },
+  {
+    "revision": "07a1072613627d02b2d3d466285cd308",
+    "url": "/updated-portfolio/static/media/pic2.07a10726.png"
+  },
+  {
+    "revision": "819ed0810d3f841f2800edaf122cd550",
+    "url": "/updated-portfolio/static/media/pic3.819ed081.png"
+  }
+]);
